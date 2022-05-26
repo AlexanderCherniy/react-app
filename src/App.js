38a7736a01016a35095389a747a3fa3f3a7a20b1
AlexanderCherniy <scernysev389@gmail.com>
2022-05-26 12:22:28 +0300

    add lazy and suspense

--- src/App.js
+++ src/App.js
@@ -3,15 +3,11 @@ import './Nullstyle.css';
 import HeaderContainer from './components/Header/HeaderContainer';
 import Help from './components/Help/Help';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
-// import MassagesContainer from './components/Massages/MassagesContainer';
 import SettingsContainer from './components/Settings/SettingsContainer';
 import SideBarContainer from './components/SideBar/SideBarContainer';
 import FriendsContainer from './components/Friends/FriendsContainer';
 import { connect, Provider } from 'react-redux'
 import GaysContainer from './components/GAYS/GaysContainer';
-import UsersContainer from './components/Users/UsersContainer';
-import ProfileContainer from './components/Profile/ProfileContainer';
-import LoginContainer from './components/Login/LoginContainer';
 import React, { useEffect } from 'react';
 import { compose } from 'redux';
 import { inizializateReady } from './redux/app-reducer';
@@ -19,6 +15,9 @@ import { setProfile } from './redux/auth-reducer';
 import store from './redux/store-redux';
 import { LazyLoaderHOC } from './noc/lazyLoad';
 const MassagesContainer = React.lazy(() => import('./components/Massages/MassagesContainer'));
+const ProfileContainer = React.lazy(() => import('./components/Profile/ProfileContainer'));
+const UsersContainer = React.lazy(() => import('./components/Users/UsersContainer'));
+const LoginContainer = React.lazy(() => import('./components/Login/LoginContainer'));
 const App = props =>{
   useEffect(()=>{
     props.inizializateReady(props.userData.isAuth, props.userData.id)
@@ -30,8 +29,14 @@ const App = props =>{
         <div className='shellMainSide'>
           <SideBarContainer store = {props.store}/>
           <Routes>
-            <Route path='/profile/:userId' element={<ProfileContainer store = {props.store}/>}/>
-            <Route path='/profile' element={<ProfileContainer store = {props.store}/>}/>
+            <Route path='/profile/:userId' element={
+              <React.Suspense fallback={<div>Загрузка...</div>}>
+                <ProfileContainer store = {props.store}/>
+              </React.Suspense>}/>
+            <Route path='/profile' element={
+              <React.Suspense fallback={<div>Загрузка...</div>}>
+                <ProfileContainer store = {props.store}/>
+              </React.Suspense>}/>
             <Route path='/music' element={<div>Music</div>}/>
             <Route path='/friends' element={<FriendsContainer/>}/>
             <Route path='/gays' element={<GaysContainer/>}/>
@@ -44,8 +49,14 @@ const App = props =>{
             {/* <Route path='/massages/*' element={LazyLoaderHOC(<MassagesContainer/>)}/> */}
             <Route path='/help' element={<Help/>}/>
             <Route path='/settings' element={<SettingsContainer store = {props.store}/>}/>
-            <Route path='/users' element={<UsersContainer store={props.store}/>}/>
-            <Route path='/login' element={<LoginContainer store={props.store}/>}/>
+            <Route path='/users' element={
+              <React.Suspense fallback={<div>Загрузка...</div>}>
+                <UsersContainer store = {props.store}/>
+              </React.Suspense>}/>
+            <Route path='/login' element={
+              <React.Suspense fallback={<div>Загрузка...</div>}>
+                <LoginContainer store = {props.store}/>
+              </React.Suspense>}/>
           </Routes>
         </div>
       </div>
